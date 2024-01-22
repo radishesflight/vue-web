dd3873ce0df2b64390b325c86ff22a8b3e2fef10
M-DuXingYu <530606930@qq.com>
2024-01-22 11:53:46 +0800

    wev

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,38 +1,111 @@
+<style>
+.layout-base {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  overflow: hidden;
+}
+
+.layout-base {
+  margin-bottom: 20px;
+}
+
+.layout-header {
+  background: #3A9FE8;
+  color: white;
+  height: 100px;
+}
+
+.layout-header div {
+  height: 100%;
+  line-height: 100px;
+  text-align: center;
+}
+
+.layout-footer {
+  background: #7CBCE9;
+  color: white;
+}
+
+.layout-content {
+  background: #0F8DE9;
+  min-height: 120px;
+  line-height: 120px;
+}
+
+.layout-sider {
+  background: #3A9FE8;
+  min-height: 120px;
+  line-height: 120px;
+}
+
+.layout iframe {
+  width: 100%;
+  border: 0;
+}
+
+.menu-item span {
+  display: inline-block;
+  overflow: hidden;
+  width: 69px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
+  transition: width .2s ease .2s;
+}
+
+.menu-item i {
+  transform: translateX(0px);
+  transition: font-size .2s ease, transform .2s ease;
+  vertical-align: middle;
+  font-size: 16px;
+}
+
+.collapsed-menu span {
+  width: 0px;
+  transition: width .2s ease;
+}
+
+.collapsed-menu i {
+  transform: translateX(5px);
+  transition: font-size .2s ease .2s, transform .2s ease .2s;
+  vertical-align: middle;
+  font-size: 22px;
+}
+</style>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div>
-      <Button type="info">Info</Button>
-      <Button type="success">Success</Button>
-      <Button type="warning">Warning</Button>
-      <Button type="error">Error</Button>
-    </div>
+  <div class="layout-base">
+    <Layout>
+      <Header class="layout-header">
+        <Row>
+          <Col span="8">
+            <div>
+              <img src="@/assets/yh-logo2.273bd23c.png"  style="padding:20px;height: 100%" alt="">
+            </div>
+          </Col>
+          <Col span="8">
+            <div>
+              dsf
+            </div>
+          </Col>
+          <Col span="8">
+            <div>
+              dsf
+            </div>
+          </Col>
+        </Row>
+      </Header>
+      <Layout>
+        <Content class="layout-content">Content</Content>
+        <Sider class="layout-sider" hide-trigger>Sider</Sider>
+      </Layout>
+      <Footer class="layout-footer">Footer</Footer>
+    </Layout>
   </div>
 </template>
 <script>
-import { Button, Table } from 'view-ui-plus';
-export default {
-  mounted() {
-    this.axios.get('/goods/ad/byPage?clientType=0&pageName=login')
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-  },
-  components:{
-    Button,
-    Table
-  },
-  created() {
-    // this.axios.get('/goods/ad/byPage?clientType=0&pageName=login')
-    //     .then(response => {
-    //       console.log(response.data);
-    //     })
-    //     .catch(error => {
-    //       console.error('Error fetching data:', error);
-    //     });
-  }
-}
+export default {}
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,122 @@
+<style>
+.header{
+  width: 100%;
+  height: 90px;
+  line-height: 90px;
+  background: white;
+  text-align: center;
+}
+.header div{
+  height: 100%;
+}
+
+.center{
+  background: #FADAEF;
+  height:calc(100vh - 200px);
+  width: 100%;
+  background-image: url("@/assets/center.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.form-login{
+  width: 30%;
+  height: 80%;
+  background: white;
+  position: relative;
+  top: 10%;
+  left: 62%;
+  border-radius: 2%;
+  text-align: center;
+}
+.tab-login{
+  height: 70px;
+  line-height: 70px;
+  border-bottom: 1px solid #CCCCCC;
+}
+.tab-login-span{
+  font-size: larger;
+  font-weight: bold;
+}
+.tab-login-span-active{
+  color: #B72C2E;
+}
+.tab-login-span-one{
+}
+.tab-login-span-two{
+}
+.demo-login{
+  width: 400px;
+  margin: 0 auto !important;
+}
+.demo-auto-login{
+  margin-bottom: 24px;
+  text-align: left;
+}
+.demo-auto-login a{
+  float: right;
+}
+</style>
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <Row>
+      <Col span="9">
+        <div>
+          <img src="@/assets/yh-logo2.273bd23c.png"  style="padding:25px;height: 100%" alt="">
+        </div>
+      </Col>
+      <Col span="6" offset="7">
+        <div>
+          <Button type="error" ghost>返回首页</Button>
+        </div>
+      </Col>
+
+    </Row>
   </div>
+  <div class="center">
+
+    <div class="form-login">
+      <div class="tab-login">
+        <Row>
+          <Col span="12" @click="toggle($event)">
+            <span class="tab-login-span tab-login-span-active" v-if="tabActive">用户名登录</span>
+            <span class="tab-login-span" v-else>用户名登录</span>
+          </Col>
+          <Col span="12" @click="toggle($event)">
+            <span class="tab-login-span tab-login-span-active" v-if="!tabActive">手机号登录</span>
+            <span class="tab-login-span" v-else>手机号登录</span>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  </div>
+  <div></div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      autoLogin: true,
+      tabActive:true,
+    }
+  },
+  methods: {
+    toggle(event){
+      this.tabActive=!this.tabActive;
+      // const clickedElement = event.target;
+      // if (clickedElement.classList.contains('tab-login-span-active')){
+      //   this.tabActive=true
+      //   // 515A6E
+      // }else {
+      //   this.tabActive=false
+      // }
+      // clickedElement.classList.add('tab-login-span-active')
+      // // 检查点击的元素是否包含指定的类
+      // if (clickedElement.classList.contains('tab-login-span')) {
+      // clickedElement.style.color="#B72C2E";
+      // } else {
+      //   console.log('Did not find the target class.');
+      // }
+    }
   }
 }
 </script>
